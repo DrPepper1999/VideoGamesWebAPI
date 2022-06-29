f1a37fe876cdeb26f8090951c555aa5760dfb8b3
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2022-06-30 00:05:04 +0300

    Add DependencyInjection to Application and Persistence

---
+++ VideoGames.Persistence.cs/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using VideoGames.Application.Interfaces;
+
+namespace VideoGames.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<VideoGamesDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<IVideoGamesDbContext>(provider =>
+                provider.GetService<VideoGamesDbContext>());
+
+            return services;
+        }
+    }
+}
